f63b6e15b1447fe43cedc506e887c46854de3e18
HypeVoidSoul <hypevoidsoul@yahoo.com>
2021-07-16 11:34:01 -0700

    Filters.chat_type.groups

--- Skars/antiflood.py
+++ Skars/antiflood.py
@@ -370,14 +370,12 @@ __help__ = "antiflood_help"
 
 __mod_name__ = "Antiflood"
 
-FLOOD_BAN_HANDLER = MessageHandler(Filters.all & ~Filters.status_update & Filters.group, check_flood)
-SET_FLOOD_HANDLER = CommandHandler("setflood", set_flood, pass_args=True)#, filters=Filters.group)
-SET_FLOOD_MODE_HANDLER = CommandHandler("setfloodmode", set_flood_mode, pass_args=True)#, filters=Filters.group)
-FLOOD_HANDLER = CommandHandler("flood", flood)#, filters=Filters.group)
-# FLOOD_BTNSET_HANDLER = CallbackQueryHandler(FLOOD_EDITBTN, pattern=r"set_flim")
+FLOOD_BAN_HANDLER = MessageHandler(Filters.all & ~Filters.status_update & Filters.chat_type.groups, check_flood)
+SET_FLOOD_HANDLER = CommandHandler("setflood", set_flood, pass_args=True, filters=Filters.chat_type.groups)
+SET_FLOOD_MODE_HANDLER = CommandHandler("setfloodmode", set_flood_mode, pass_args=True, filters=Filters.chat_type.groups)
+FLOOD_HANDLER = CommandHandler("flood", flood, filters=Filters.chat_type.groups)
 
 dispatcher.add_handler(FLOOD_BAN_HANDLER, FLOOD_GROUP)
 dispatcher.add_handler(SET_FLOOD_HANDLER)
 dispatcher.add_handler(SET_FLOOD_MODE_HANDLER)
 dispatcher.add_handler(FLOOD_HANDLER)
-# dispatcher.add_handler(FLOOD_BTNSET_HANDLER)
